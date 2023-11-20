619e5940337d3d3d2626b4b0144a12ea13e58455
Raul Andrial <rauljandrial@gmail.com>
2023-11-19 19:49:58 -0500

    created intents_to_json.py to more easily edit intents file

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
 EXAMPLE_INTENTS = 'example.intents.json'
 INTENTS = 'intents.json'
+EXAMPLE_MODEL_DATA = 'example.data.pth'
+MODEL_DATA = 'data.pth'
---
+++ intents_to_json.py
@@ -0,0 +1,8 @@
+import json
+from intents import intents
+
+print(intents['intents'])
+
+# Convert dictionary to json object and write to intents.json
+# with open('intents.json', 'w') as f:
+#     json.dump(intents, f)
